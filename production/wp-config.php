e5134917f6bc8170ba047e0cb7d20037bc27f507
Max Humberstone <maxhumberstone@Maxs-MacBook-Pro.local>
2019-12-17 23:29:56 +0000

    update progress bar colour

--- production/wp-config.php
+++ production/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'portfolio19_website');
+define('DB_NAME', 'porfolio19');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'humbm0');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'QB90VkfzsGScJgU0');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
